2eb03e38ce516b0bfa6b65619c394060d61289d1
nimoh <spakers38@gmail.com>
2024-05-27 21:34:02 +0900

    feat: env.properties 파일을 스프링부트에서 사용하기 위한 설정 파일 추가

---
+++ src/main/java/com/devleoh/glomo/config/env/EnvConfig.java
@@ -0,0 +1,20 @@
+package com.devleoh.glomo.config.env;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * packageName    : com.devleoh.glomo.config.env
+ * fileName       : EnvConfig
+ * author         : nimoh
+ * date           : 2024/05/27
+ * description    : env.properties 파일을 스프링 Config에 세팅
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024/05/27        nimoh       최초 생성
+ */
+@Configuration
+@PropertySource("classpath:env.properties")
+public class EnvConfig {
+}
