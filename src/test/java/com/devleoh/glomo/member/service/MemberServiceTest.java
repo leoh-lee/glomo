d350588bbc5ed0b1b2f51f3ade446cc4afe565fa
nimoh <spakers38@gmail.com>
2024-07-29 23:13:54 +0900

    chore: github action 환경에서 H2 데이터베이스를 사용하도록 수정

--- src/test/java/com/devleoh/glomo/member/service/MemberServiceTest.java
+++ src/test/java/com/devleoh/glomo/member/service/MemberServiceTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.security.NoSuchAlgorithmException;
@@ -30,6 +31,7 @@ import static org.mockito.Mockito.when;
  * 2024/06/24        nimoh       최초 생성
  */
 @SpringBootTest
+@ActiveProfiles("test")
 @Transactional
 public class MemberServiceTest {
 
